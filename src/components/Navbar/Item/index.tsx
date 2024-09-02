a18a9a7cab6479e12e0f5767812c23c172c4cef9
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-02 02:17:24 -0300

    feat: implement navbar item

--- src/components/Navbar/Item/index.tsx
+++ src/components/Navbar/Item/index.tsx
@@ -2,16 +2,16 @@ import { NavbarItemProps } from "./types";
 
 const activeStyles = {
   active: 'text-leaf-500',
-  default: '',
+  default: 'text-slate-50',
 }
 
 export const NavbarItem = ({ label, href, isActive }: NavbarItemProps) => {
 
   return (
-    <li className={`${isActive ? activeStyles.active : ''} hover:text-leaf-500 `}>
+    <li className={`${isActive ? activeStyles.active : activeStyles.default} hover:text-leaf-500 `}>
       <a
         href={href}
-        className="text-sm font-medium text-gray-900 hover:text-gray-700 dark:text-gray-300"
+        className="text-sm font-medium"
       >
         {label}
       </a>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import { NavbarItem } from "./Item";
 import { NavbarProps } from "./types";
 
 export const Navbar = ({ ...rest }: NavbarProps) => {
@@ -7,9 +8,9 @@ export const Navbar = ({ ...rest }: NavbarProps) => {
         <img src="" alt="Logo" />
       </a>
       <ul className="list-none flex gap-20">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <NavbarItem label="Home" href="/" isActive={true}/>
+        <NavbarItem label="About" href="/about" isActive={false}/>
+        <NavbarItem label="Contact" href="/contact" isActive={false}/>
       </ul>
     </nav>
   );
