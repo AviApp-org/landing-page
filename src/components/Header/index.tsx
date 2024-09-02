4f55fec4b33e3f0ff5b0ad142fb633972127c447
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-02 02:57:24 -0300

    refactor: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../Navbar";
 
 export const Header = () => {
   return (
-    <header className="w-full flex justify-between absolute bg-transparent backdrop-blur-md">
+    <header className="w-full flex justify-between absolute bg-transparent">
       <Navbar />
     </header>
   );
