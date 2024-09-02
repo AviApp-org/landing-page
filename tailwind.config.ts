63a55bd621707a9707372f48cc629dc0bfbd24af
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-02 02:16:39 -0300

    chore: add app colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        leaf: {
+          100: "#e8f0d6",
+          200: "#d0e1ad",
+          300: "#b9d185",
+          400: "#a1c25c",
+          500: "#8ab333",
+          600: "#6e8f29",
+          700: "#536b1f",
+          800: "#374814",
+          900: "#1c240a",
+        },
+      },
     },
   },
   plugins: [],
