eea6441e29fce322143ccfc36ae61c8c2756227f
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-05 12:52:08 -0300

    fix: text position on banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,9 +2,13 @@ import { BannerProps } from "./types";
 
 export const Banner = ({ ...rest }: BannerProps) => {
   return (
-    <div {...rest} className={`w-full h-[600px] object-cover flex ${rest.className}`}>
-      <h1 className="text-7xl text-left font-bold text-slate-200 absolute self-end font-sans left-20 bottom-20 leading-snug">
-        A ferramenta de gestão <br /> para <mark className="bg-leaf-600 text-slate-200 px-4">sua granja!</mark>
+    <div
+      {...rest}
+      className={`w-full h-[600px] flex ${rest.className}`}
+    >
+      <h1 className="text-6xl text-left font-bold text-slate-100 absolute self-end font-sans leading-snug mb-10 ml-20">
+        A ferramenta de gestão <br /> para{" "}
+        <mark className="bg-leaf-600 text-slate-200 px-4">sua granja!</mark>
       </h1>
       <img
         src="banner-2.jpeg"
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import { NavbarProps } from "./types";
 
 export const Navbar = ({ ...rest }: NavbarProps) => {
   return (
-    <nav {...rest} className={`w-full bg-transparent flex justify-between ${rest.className} p-4`}>
+    <nav {...rest} className={`w-full bg-transparent flex justify-between ${rest.className} px-8 py-4`}>
       <a href="#">
         <img src="" alt="Logo" />
       </a>
@@ -11,7 +11,7 @@ export const Navbar = ({ ...rest }: NavbarProps) => {
         <NavbarItem label="Home" href="/" isActive={true}/>
         <NavbarItem label="About" href="/about" isActive={false}/>
         <NavbarItem label="Contact" href="/contact" isActive={false}/>
-        <NavbarItem label="Products" href="/products" isActive={false} className="px-4 py-2 bg-leaf-500 rounded" />
+        <NavbarItem label="Products" href="/products" isActive={false} className="px-4 py-2 bg-leaf-500 rounded hover:text-slate-50" />
       </ul>
     </nav>
   );
