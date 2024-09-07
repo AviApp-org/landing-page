7ef43e1a080a6ad98d9bbc451bf5f1178c45c8b9
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-07 05:25:23 -0300

    feat: add sections to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Banner } from "@/components/Banner";
 import { Header } from "@/components/Header";
+import { SectionAbout } from "@/patterns/SectionAbout";
+import { SectionPricing } from "@/patterns/SectionPricing";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center w-full">
       <Header />
       <Banner />
-      {/* <SectionAbout /> */}
+      <SectionAbout />
+      <SectionPricing />
     </main>
   );
 }
