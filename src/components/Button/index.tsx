3ba76cc80f6d814d0016210147600894b39f3621
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-12 10:02:25 -0300

    refactor: component styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ export const Button = ({ text, className, ...rest }: ButtonProps) => {
   return (
     <button
       {...rest}
-      className={`bg-leaf-600 text-slate-50 font-sans font-semibold text rounded-lg px-4 py-2 hover:bg-leaf-700 ${className}`}
+      className={`bg-leaf-600 text-slate-50 font-sans font-semibold text rounded-lg px-4 py-2 hover:bg-leaf-700 max-sm:py-0 max-sm:text-sm ${className}`}
     >
       {text}
     </button>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,13 +2,13 @@ import { InputProps } from "./types";
 
 export const Input = ({ type, label, ...rest }: InputProps) => {
   return (
-    <div className="flex flex-col gap-1 w-full">
-      <label htmlFor={rest.id} className={`block font-bold text-gray-50 ${rest.className}`}>{label}</label>
+    <div className="flex flex-col gap-1 w-full max-sm:text-sm">
+      <label htmlFor={rest.id} className={`block font-bold text-gray-50 max-sm:text-sm ${rest.className}`}>{label}</label>
       <input
         type={type}
         id={rest.id}
         {...rest}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 font-sans"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 font-sans max-sm:text-sm"
         placeholder="seuemail@provedor.com"
       />
     </div>
