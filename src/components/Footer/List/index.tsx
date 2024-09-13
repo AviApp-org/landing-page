dd170d9f9bd1d996e35e37e882179d978b7a65c4
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-13 10:03:17 -0300

    refactor: footer styles

--- src/components/Footer/List/index.tsx
+++ src/components/Footer/List/index.tsx
@@ -2,7 +2,7 @@ import { FooterListProps } from "./types";
 
 export const FooterList = ({ title, children }: FooterListProps) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 max-sm:gap-0">
       <h4 className="font-semibold text-slate-50 text-xl">{title}</h4>
       <ul className="list-none">{children}</ul>
     </div>
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,27 +3,34 @@ import { FooterListItem } from "./List/Item";
 
 export const Footer = () => {
   return (
-    <footer className="bg-leaf-600 w-full h-96 p-12 flex flex-row justify-between">
-      <div className="w-full">
-        <img
-          src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Logo"
-          className="w-20 h-20"
-        />
+    <footer className="bg-leaf-600 w-full h-96 flex flex-col items-center justify-between max-sm:min-h-fit max-sm:h-full max-sm:gap-0">
+      <div className="w-full flex-row items-center justify-between max-sm:flex-col">
+        <div className="w-full p-12 max-sm:p-8 max-sm:pb-0">
+          <img
+            src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Logo"
+            className="w-20 h-20"
+          />
+        </div>
+        <div className="w-2/3 flex justify-evenly gap-18 max-sm:flex-col max-sm:w-full max-sm:gap-6 p-12 max-sm:p-8">
+          <FooterList title="Contact">
+            <FooterListItem label="E-Mail" link="" />
+            <FooterListItem label="Phone" link="" />
+          </FooterList>
+          <FooterList title="Products">
+            <FooterListItem label="AviApp" link="" />
+          </FooterList>
+          <FooterList title="About-us">
+            <FooterListItem label="Company" link="" />
+            <FooterListItem label="Privacy" link="" />
+            <FooterListItem label="Terms" link="" />
+          </FooterList>
+        </div>
       </div>
-      <div className="w-2/3 flex justify-evenly gap-18">
-        <FooterList title="Contact">
-          <FooterListItem label="E-Mail" link="" />
-          <FooterListItem label="Phone" link="" />
-        </FooterList>
-        <FooterList title="Products">
-          <FooterListItem label="AviApp" link="" />
-        </FooterList>
-        <FooterList title="About-us">
-          <FooterListItem label="Company" link="" />
-          <FooterListItem label="Privacy" link="" />
-          <FooterListItem label="Terms" link="" />
-        </FooterList>
+      <div className="w-full border-t border-leaf-700 p-4">
+        <p className="font-sans text-leaf-700 text-center">
+          All rights reserved © 2022 AviApp
+        </p>
       </div>
     </footer>
   );
