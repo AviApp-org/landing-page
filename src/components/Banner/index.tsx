b3ab5c7d6ecf9e52c5c0d199ba68bc4b2d2e7a8e
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-26 20:04:22 -0300

    fix: component width

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,7 +6,7 @@ export const Banner = ({ ...rest }: BannerProps) => {
       {...rest}
       className={`w-full h-[650px] max-sm:h-[500px] flex max-sm:justify-center max-sm:items-center ${rest.className}`}
     >
-      <h1 className="text-6xl text-left font-bold text-slate-100 absolute self-end font-sans leading-snug mb-10 ml-20 max-sm:text-5xl max-sm:text-center max-sm:self-center max-sm:m-0 max-sm:w-[90%] max-sm:leading-normal max-md:block">
+      <h1 className="text-6xl text-left font-bold text-slate-100 absolute self-end font-sans leading-snug mb-10 ml-20 max-sm:text-5xl max-sm:text-center max-w-screen-xl max-sm:self-center max-sm:m-0 max-sm:w-[90%] max-sm:leading-normal max-md:block">
         A ferramenta de gestão <br /> para a sua {" "}
         <mark className="bg-leaf-600 text-slate-200 px-4">granja!</mark>
       </h1>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -11,24 +11,16 @@ export const Navbar = ({ ...rest }: NavbarProps) => {
   return (
     <nav
       {...rest}
-      className={`w-full backdrop-blur border-gray-200 flex items-center justify-between ${rest.className} px-8 py-2 max-md:items-center max-md:flex-col max-md:gap-4`}
+      className={`w-full backdrop-blur border-gray-200 flex items-center justify-center ${rest.className} px-8 py-2 max-md:items-center max-md:flex-col max-md:gap-4`}
     >
-      <div className="w-full flex items-center justify-between max-md:flex-row">
-        <a href="#">
-          <AviappLogoIcon color="white" className="h-12 w-auto"/>
-        </a>
-        <HamburgerMenu onClick={() => setIsOpen(!isOpen)} />
-      </div>
-      <div className="w-full max-md:hidden md:w-auto" id="navbar-default">
-        <ul className="list-none flex items-center gap-14 max-md:flex-col max-md:gap-0 max-md:p-2 max-md:bg-gray-100 max-md:rounded-md">
-          <NavbarItem label="Home" href="/" isActive={true} />
-          <NavbarItem label="About" href="/about" isActive={false} />
-          <NavbarItem label="Contact" href="/contact" isActive={false} />
-          <NavbarItem label="Products" href="/products" isActive={false} />
-        </ul>
-      </div>
-      {isOpen && (
-        <div className="w-full md:block md:w-auto" id="navbar-default">
+      <div className="w-full max-w-screen-xl flex items-center justify-between">
+        <div className="w-full flex items-center justify-between max-md:flex-row">
+          <a href="#">
+            <AviappLogoIcon color="white" className="h-12 w-auto" />
+          </a>
+          <HamburgerMenu onClick={() => setIsOpen(!isOpen)} />
+        </div>
+        <div className="w-full max-md:hidden md:w-auto" id="navbar-default">
           <ul className="list-none flex items-center gap-14 max-md:flex-col max-md:gap-0 max-md:p-2 max-md:bg-gray-100 max-md:rounded-md">
             <NavbarItem label="Home" href="/" isActive={true} />
             <NavbarItem label="About" href="/about" isActive={false} />
@@ -36,7 +28,17 @@ export const Navbar = ({ ...rest }: NavbarProps) => {
             <NavbarItem label="Products" href="/products" isActive={false} />
           </ul>
         </div>
-      )}
+        {isOpen && (
+          <div className="w-full md:block md:w-auto" id="navbar-default">
+            <ul className="list-none flex items-center gap-14 max-md:flex-col max-md:gap-0 max-md:p-2 max-md:bg-gray-100 max-md:rounded-md">
+              <NavbarItem label="Home" href="/" isActive={true} />
+              <NavbarItem label="About" href="/about" isActive={false} />
+              <NavbarItem label="Contact" href="/contact" isActive={false} />
+              <NavbarItem label="Products" href="/products" isActive={false} />
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
