e4251d58da051cd980c276a12907ca595d390378
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-29 20:56:11 -0300

    feat: add section feedback

---
+++ src/patterns/SectionFeedback/index.tsx
@@ -0,0 +1,19 @@
+import { CardFeedback } from "@/components/Cards/Feedback";
+import { reviews } from "@/components/Cards/Feedback/types";
+import { SectionHeader } from "@/components/SectionHeader";
+
+export const SectionFeedback = () => {
+  return (
+    <section className="w-full h-full flex items-center justify-center py-12">
+      <div className="w-full h-full max-w-7xl flex flex-col items-center justify-center gap-20 max-sm:gap-2">
+        <SectionHeader title="O que nossos clientes dizem" />
+
+        <div className="w-full overflow-scroll flex items-start justify-center gap-2 max-w-4xl max-sm:flex-col max-sm:p-10 max-sm:gap-6">
+          {reviews.map((review, key) => {
+            return <CardFeedback review={review} key={key} />
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
