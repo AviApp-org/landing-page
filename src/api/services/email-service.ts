c091623565509a93d0288b48d350365326b8c904
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-11-18 16:04:06 -0300

    refactor: code refactor

--- src/api/services/email-service.ts
+++ src/api/services/email-service.ts
@@ -5,8 +5,8 @@ export const EmailService = {
     const response = await apiClient.post(
       "/api/HttpTrigger?code=PeVxta7u5QnutJAcjjeG6bJYrzfFjuq-JPZ3oyBJYFBdAzFuI0Vxug%3D%3D",
       JSON.stringify({
-        Email: email,
-        Content: content,
+        toMail: email,
+        content: content,
       }),
       {
         headers: {
